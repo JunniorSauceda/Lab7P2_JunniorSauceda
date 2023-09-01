5c1958ea68a78450f93e0740f1c911bdcd9c6160
Junnior Sauceda <junnior.sauceda05@unitec.edu.hn>
2023-09-01 13:56:24 -0600

    Creacion de la clase Venta

---
+++ src/lab7p2_junniorsauceda/Venta.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7p2_junniorsauceda;
+
+/**
+ *
+ * @author Junnior Sauceda
+ */
+public class Venta {
+    private Vendedor vendedor=new Vendedor();
+    private Cliente comprador=new Cliente();
+    private Vehiculo carro=new Vehiculo();
+    private double Coste;
+
+    public Venta() {
+        
+    }
+
+    public Venta(double Coste) {
+        this.Coste = Coste;
+    }
+    
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public Cliente getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(Cliente comprador) {
+        this.comprador = comprador;
+    }
+
+    public Vehiculo getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Vehiculo carro) {
+        this.carro = carro;
+        setCoste(carro.getPrecio());
+    }
+
+    public double getCoste() {
+        return Coste;
+    }
+
+    public void setCoste(double Coste) {
+        this.Coste = Coste;
+    }
+    
+    
+    
+}
